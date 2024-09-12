5147c0a2c3c7adb56156e77aa58b7dab6d49ef0a
james <jgric2@gmail.com>
2024-09-13 01:06:15 +1000

    final console fix

--- FastPersistentDictionary.Console/Program.cs
+++ FastPersistentDictionary.Console/Program.cs
@@ -11,12 +11,12 @@ internal class Program
         //path: pathMain,
         Stopwatch stopwatch = new Stopwatch();
         stopwatch.Start();
-        for (int i = 0; i < 1000; i++) 
+        for (int i = 0; i < 10000; i++) 
         {
             fastPersistentDictionary.Add("name_" + i.ToString(), strValueTest);
 
         }
-        for (int i = 0; i < 1000; i++)
+        for (int i = 0; i < 10000; i++)
         {
             fastPersistentDictionary.TryGetValue("name_" + i.ToString(), out var val);
 
@@ -24,21 +24,21 @@ internal class Program
         }
 
 
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < 1000; i++)
         {
             fastPersistentDictionary.Remove("name_" + i.ToString());
 
 
         }
 
-        for (int i = 1001; i < 1100; i++)
+        for (int i = 10001; i < 10100; i++)
         {
             fastPersistentDictionary.Add("name_" + i.ToString(), strValueTest);
 
 
         }
 
-        fastPersistentDictionary.SaveDictionary("C:\\Users\\Admin\\source\\repos\\main Pronamics Work\\Fast-Persistent-Dictionary\\FastPersistentDictionary.Console\\bin\\Debug\\net8.0\\testMMM2.tt");
+        
 
         fastPersistentDictionary.Dispose();
 
